6462e499c3063ac8a4fbc95521f902fd5eaa6ad5
Anna Tsepilova <virgo.tsav@gmail.com>
2024-06-07 21:54:40 +0300

    add common buttons components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
-import Button from "./components/Button/Button";
+import {
+  AddIngrButton,
+  DeleteButton,
+  FavoriteButton,
+  DetailsButtonRecipe,
+  AddToFavButton,
+} from "./components/Buttons/Buttons";
 import MainTitle from "./components/MainTitle/MainTitle";
 import Subtitle from "./components/Subtitle/Subtitle";
 
 function App() {
   return (
     <>
-      <Button text="button" />
+      <DeleteButton />
+      <FavoriteButton />
+      <DetailsButtonRecipe />
+      <AddToFavButton text="Add to favorites" />
+      <AddIngrButton text="Add ingredient" />
       <MainTitle text="Categories" />
       <Subtitle text="Discover a limitless world of culinary possibilities and enjoy exquisite recipes that combine taste, style and the warm atmosphere of the kitchen." />
     </>
