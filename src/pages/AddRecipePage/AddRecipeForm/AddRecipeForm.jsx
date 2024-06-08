85fea20987704820c4189ed0a3e0027674ae9d63
Anna Tsepilova <virgo.tsav@gmail.com>
2024-06-08 16:15:52 +0300

    add responsive and  adaptive markup

--- src/pages/AddRecipePage/AddRecipeForm/AddRecipeForm.jsx
+++ src/pages/AddRecipePage/AddRecipeForm/AddRecipeForm.jsx
@@ -1,7 +1,8 @@
 import { useForm } from "react-hook-form";
-import styles from "./AddRecipeForm.module.css";
 
+import styles from "./AddRecipeForm.module.css";
 import sprite from "../../../assets/img/icons-sprite.svg";
+
 import {
   DeleteButton,
   AddIngrButton,
@@ -27,89 +28,103 @@ const AddRecipeForm = () => {
         </svg>
         <p className={styles.uploadPhotoText}>Upload a photo</p>
       </div>
-      <input
-        type="text"
-        {...register("recipeName", { required: true })}
-        placeholder="THE NAME OF THE RECIPE"
-        className={styles.recipeName}
-      />
+      <div className={styles.formOptionals}>
+        <input
+          type="text"
+          {...register("recipeName", { required: true })}
+          placeholder="THE NAME OF THE RECIPE"
+          className={styles.recipeName}
+        />
 
-      <div className={`${styles.inputCountWrapper} ${styles.decorLine}`}>
-        <input
-          type="text"
-          {...register("recipeDescr", { required: true }, { maxLength: 220 })}
-          placeholder="Enter a description of the dish"
-          className={` ${styles.inputArea} ${styles.inputText}`}
-        />
-        <p className={styles.symbCounter}>0/200</p>
-      </div>
-      <div className={styles.categoryTimeWrapper}>
-        <label htmlFor="category">Category</label>
-        <select {...register("category", { required: true })}>
-          <option value="beef">Beef</option>
-          <option value="breakfast">Breakfast</option>
-          <option value="desserts">Desserts</option>
-          <option value="lamb">Lamb</option>
-          <option value="miscellaneous">Miscellaneous</option>
-          <option value="pasta">Pasta</option>
-          <option value="pork">Pork</option>
-          <option value="seafood">Seafood</option>
-          <option value="side">Side</option>
-          <option value="starter">Starter</option>
-        </select>
-        <label htmlFor="">COOKING TIME</label>
-        <div className={styles.counter}>
-          <MinusButton />
-          <span className={styles.counterNum}>10 min</span>
-          <PlusButton />
-        </div>
-      </div>
-      <div className={`${styles.ingredientsWrapper} ${styles.decorLine}`}>
-        <label htmlFor="ingredients">Ingredients</label>
-        <select {...register("ingredients", { required: true })}>
-          <option value="Cabbage">Cabbage</option>
-          <option value="Cucumber">Cucumber</option>
-          <option value="desserts">Desserts</option>
-          <option value="Lamb">Lamb</option>
-          <option value="Miscellaneous">Miscellaneous</option>
-          <option value="Pasta">Pasta</option>
-          <option value="Pork">Pork</option>
-          <option value="Seafood">Seafood</option>
-          <option value="Side">Side</option>
-          <option value="Starter">Starter</option>
-        </select>
-        <input
-          type="text"
-          {...register("ingredientsQuantity", { required: true })}
-          placeholder="Enter quantity"
-          className={`${styles.ingredientsQuantity} ${styles.inputText}`}
-        />
-      </div>
-      <AddIngrButton text="Add ingredient" />
-      {/* <button type="button" className={styles.addIngrBtn}>
-        Add ingredient
-        <svg width={20} height={20} className={styles.svgPlusAdd}>
-          <use href={`${sprite}#plus`}></use>
-        </svg>
-      </button> */}
-      <div className={`${styles.recipePreparation} ${styles.decorLine}`}>
-        <label htmlFor="recipePreparation">Recipe Preparation</label>
-        <div className={styles.inputCountWrapper}>
-          <textarea
-            {...register(
-              "recipePreparation",
-              { required: true },
-              { maxLength: 220 }
-            )}
-            placeholder="Enter recipe"
-            className={`${styles.inputArea} ${styles.inputText}`}
+        <div className={styles.formOptionsWrapper}>
+          <div className={`${styles.inputAreaWrapper} ${styles.recipeDescr}`}>
+            <input
+              type="text"
+              {...register(
+                "recipeDescr",
+                { required: true },
+                { maxLength: 220 }
+              )}
+              placeholder="Enter a description of the dish"
+              className={` ${styles.inputArea} text`}
+            />
+            <p className={`${styles.symbCounter} text`}>0/200</p>
+          </div>
+          <div className={styles.addOptionsWrapper}>
+            <label htmlFor="category">Category</label>
+            <select
+              {...register("category", { required: true })}
+              placeholder="Select a category"
+            >
+              <option value="beef">Beef</option>
+              <option value="breakfast">Breakfast</option>
+              <option value="desserts">Desserts</option>
+              <option value="lamb">Lamb</option>
+              <option value="miscellaneous">Miscellaneous</option>
+              <option value="pasta">Pasta</option>
+              <option value="pork">Pork</option>
+              <option value="seafood">Seafood</option>
+              <option value="side">Side</option>
+              <option value="starter">Starter</option>
+            </select>
+          </div>
+
+          <div className={styles.addOptionsWrapper}>
+            <label htmlFor="">COOKING TIME</label>
+            <div className={styles.counter}>
+              <MinusButton />
+              <span className={`${styles.counterNum} text`}>10 min</span>
+              <PlusButton />
+            </div>
+          </div>
+
+          <div className={styles.addOptionsWrapper}>
+            <label htmlFor="ingredients">Ingredients</label>
+            <select
+              {...register("ingredients", { required: true })}
+              placeholder="Add the ingredient"
+            >
+              <option value="Cabbage">Cabbage</option>
+              <option value="Cucumber">Cucumber</option>
+              <option value="desserts">Desserts</option>
+              <option value="Lamb">Lamb</option>
+              <option value="Miscellaneous">Miscellaneous</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Pork">Pork</option>
+              <option value="Seafood">Seafood</option>
+              <option value="Side">Side</option>
+              <option value="Starter">Starter</option>
+            </select>
+          </div>
+
+          <input
+            type="text"
+            {...register("ingredientsQuantity", { required: true })}
+            placeholder="Enter quantity"
+            className={`${styles.ingredientsQuantity} text`}
           />
-          <p className={styles.symbCounter}>0/200</p>
         </div>
-      </div>
-      <div className={styles.bottomBtns}>
-        <DeleteButton />
-        <input type="submit" className={styles.submitBtn} />
+        <AddIngrButton text="Add ingredient" />
+
+        <div className={styles.recipePreparation}>
+          <label htmlFor="recipePreparation">Recipe Preparation</label>
+          <div className={styles.inputAreaWrapper}>
+            <textarea
+              {...register(
+                "recipePreparation",
+                { required: true },
+                { maxLength: 220 }
+              )}
+              placeholder="Enter recipe"
+              className={`${styles.inputArea} text`}
+            />
+            <p className={`${styles.symbCounter} text`}>0/200</p>
+          </div>
+        </div>
+        <div className={styles.bottomBtns}>
+          <DeleteButton />
+          <input type="submit" className={styles.submitBtn} />
+        </div>
       </div>
     </form>
   );
