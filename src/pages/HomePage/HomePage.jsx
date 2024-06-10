9466b092a5b8d83ec3d02cb19ebf6a5cd03e4c6e
NataliiaValko <tanarisoffice@gmail.com>
2024-06-10 03:20:44 +0300

    add home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,49 @@
+import { useEffect, useState } from "react";
+
 import Hero from "../../components/Hero/Hero.jsx";
 import Categories from "../../components/Categories/Categories.jsx";
 import Recipes from "../../components/Recipes/Recipes.jsx";
 import Testimonials from "../../components/Testimonials/Testimonials.jsx";
+import { getTestimonials } from "../../services/testimonials.js";
 
 const HomePage = () => {
+  const [selectedCategories, setSelectedCategories] = useState(null);
+  const [error, setError] = useState(null);
+  const [testimonials, setTestimonials] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await getTestimonials();
+        setTestimonials(result);
+      } catch (error) {
+        setError(error.message);
+      }
+    })();
+  }, []);
+
+  const handleSelectedCategory = (categoryName) => {
+    setSelectedCategories(categoryName);
+  };
+  const handleBack = () => {
+    setSelectedCategories(null);
+  };
+
   return (
     <>
       <Hero />
-      <Categories />
-      <Recipes />
-      <Testimonials />
+      {selectedCategories ? (
+        <Recipes selectedCategories={selectedCategories} onBack={handleBack} />
+      ) : (
+        <Categories onSelectedCategory={handleSelectedCategory} />
+      )}
+
+      {Array.isArray(testimonials) && (
+        <Testimonials testimonials={testimonials} />
+      )}
+
+      {/* Поміняти на компонент нотифікашки */}
+      {error && <p>{error}</p>}
     </>
   );
 };
