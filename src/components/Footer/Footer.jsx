8b771080af83f9d93048af103e6b1466fcd30533
Oleksandr <workbov@gmail.com>
2024-06-11 13:03:15 +0200

    add footer style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,20 +3,21 @@ import css from "./Footer.module.css";
 const Footer = () => {
   return <footer className={css.footer}>
     <div className={css.footerContent}>
-      <a href="#" className={css.logoFooter}>logo-footer</a>
+      <a href="#" className={css.logoFooter}>Foodies</a>
       <ul className={css.footerSocialList}>
-        <li>
-        <a href="https://www.facebook.com/goITclub/">facebook</a>
+        <li className={css.footerSocialItem}>
+        <a href="https://www.facebook.com/goITclub/" target="_blank"><svg><use></use></svg></a>
         </li>
-        <li>
-        <a href="https://www.instagram.com/goitclub/">Instagram</a>
+        <li className={css.footerSocialItem}>
+        <a href="https://www.instagram.com/goitclub/" target="_blank"><svg><use></use></svg></a>
         </li>
-        <li>
-        <a href="https://www.youtube.com/c/GoIT">youtube</a>
+        <li className={css.footerSocialItem}>
+          <a href="https://www.youtube.com/c/GoIT"
+            target="_blank"><svg><use></use></svg></a>
         </li>
       </ul>
     </div>
-    <div>
+    <div className={css.footerCopyrightBlock}>
       <p className={css.footerCopyright}>@2024, Foodies. All rights reserved</p>
     </div>
   </footer>;
