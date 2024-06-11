9b048adc63a17a5cccf02cf880801057c6b44fed
Oleksandr <workbov@gmail.com>
2024-06-11 11:42:42 +0200

    add component footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Button from "./components/Button/Button";
 import MainTitle from "./components/MainTitle/MainTitle";
 import Subtitle from "./components/Subtitle/Subtitle";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Button text="button" />
       <MainTitle text="Categories" />
       <Subtitle text="Discover a limitless world of culinary possibilities and enjoy exquisite recipes that combine taste, style and the warm atmosphere of the kitchen." />
+      <Footer />
     </>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,25 @@
+import css from "./Footer.module.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer className={css.footer}>
+    <div className={css.footerContent}>
+      <a href="#" className={css.logoFooter}>logo-footer</a>
+      <ul className={css.footerSocialList}>
+        <li>
+        <a href="https://www.facebook.com/goITclub/">facebook</a>
+        </li>
+        <li>
+        <a href="https://www.instagram.com/goitclub/">Instagram</a>
+        </li>
+        <li>
+        <a href="https://www.youtube.com/c/GoIT">youtube</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <p className={css.footerCopyright}>@2024, Foodies. All rights reserved</p>
+    </div>
+  </footer>;
 };
 
 export default Footer;
