9d6b5fe23e668c6a9e9b5111289279dcbe606e67
Serhii Osmolovskyi <osmolovsky.s@gmail.com>
2024-06-12 11:29:05 -0600

    Categories templates

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,5 +1,24 @@
-const Categories = () => {
-  return <div>Categories</div>;
+import MainTitle from "../MainTitle/MainTitle.jsx";
+import SubTitle from "../Subtitle/Subtitle.jsx";
+import CategoryList from "../CategoryList/CategoryList.jsx";
+import styles from "./Categories.module.css";
+
+const Categories = ({ categories }) => {
+  console.log(categories);
+  return (
+    <div>
+      <MainTitle text="Categories" />
+      <SubTitle
+        text="Discover a limitless world of culinary possibilities
+       and enjoy exquisite recipes that combine taste, style and the warm atmosphere of the kitchen."
+      />
+      <ul className={styles.categoriesGrid}>
+        {categories.map(({ _id, name, image }, index) => (
+          <CategoryList key={_id} name={name} image={image} index={index} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Categories;
