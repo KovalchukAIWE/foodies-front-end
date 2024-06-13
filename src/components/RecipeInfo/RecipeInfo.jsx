354ba52462e63d4fd2c6b0e533b33ac1c0f3491d
Oleksandr <workbov@gmail.com>
2024-06-13 21:05:28 +0200

    component render RecipeInfo

--- src/components/RecipeInfo/RecipeInfo.jsx
+++ src/components/RecipeInfo/RecipeInfo.jsx
@@ -1,5 +1,90 @@
-const RecipeInfo = () => {
-  return <div>RecipeInfo</div>;
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { getDataRecipeById } from '../../services/recipes';
+import Modal from '../Modal/Modal';
+import SignInModal from '../SignInModal/SignInModal';
+import { AddToFavButton } from '../Buttons/Buttons';
+import css from './RecipeInfo.module.css';
+import RecipeIngredients from '../RecipeIngredients/RecipeIngredients';
+
+const RecipeInfo = ({ isAuthenticated }) => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [recipe, setRecipe] = useState(null);
+  const [showSignInModal, setShowSignInModal] = useState(false);
+
+  useEffect(() => {
+    getDataRecipeById(id)
+      .then((data) => {
+        setRecipe(data);
+      })
+      .catch((err) => {
+        console.error('Failed to fetch recipe', err);
+      });
+  }, [id]);
+
+  const handleAuthorClick = () => {
+    if (isAuthenticated) {
+      navigate(`/user/${recipe.owner}`);
+    } else {
+      setShowSignInModal(true);
+    }
+  };
+
+  if (!recipe) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={css.recipeInfo}>
+      <img
+        src={recipe.thumb}
+        alt={recipe.title}
+        className={css.recipeImage}
+      />
+      <div className={css.recipeDetails}>
+        <div className={css.recipeTitleAndAuthor}>
+          <h2 className={css.recipeTitle}>{recipe.title}</h2>
+          <div className={css.categoryAndTime}>
+            <p className={css.recipeCategory}>{recipe.category}</p>
+            <p className={css.recipeTime}>{recipe.time} min</p>
+          </div>
+          <p className={css.recipeDescription}>{recipe.description}</p>
+          <button
+            onClick={handleAuthorClick}
+            className={css.recipeAuthor}>
+            Created by {recipe.owner}
+          </button>
+        </div>
+        <div className={css.recipeDetailsIngredients}>
+          <h3 className={css.recipeTitleIngredients}>Ingredients</h3>
+          <ul className={css.recipeIngredientsList}>
+            {recipe.ingredients.map((ingredient) => (
+              <RecipeIngredients
+                key={ingredient.id}
+                id={ingredient.id}
+                ingredient={ingredient}
+              />
+            ))}
+          </ul>
+        </div>
+        <div className={css.recipeDetailsInstructions}>
+          <h3 className={css.recipeTitleInstructions}>Recipe Preparation</h3>
+          <p className={css.recipeInstructions}>{recipe.instructions}</p>
+        </div>
+        <AddToFavButton
+          recipe={recipe}
+          className={css.addToFavButton}
+        />
+
+        {showSignInModal && (
+          <Modal onClose={() => setShowSignInModal(false)}>
+            <SignInModal />
+          </Modal>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default RecipeInfo;
--- src/components/RecipeIngredients/RecipeIngredients.jsx
+++ src/components/RecipeIngredients/RecipeIngredients.jsx
@@ -1,5 +1,21 @@
-const RecipeIngredients = () => {
-  return <div>RecipeIngredients</div>;
+import styles from './RecipeIngredients.module.css';
+// import getIngredients from '../../services/ingredients';
+
+// console.log(getIngredients('640c2dd963a319ea671e382c'));
+const RecipeIngredients = ({ ingredient }) => {
+  return (
+    <li className={styles.ingrItem}>
+      <img
+        className={styles.ingrImg}
+        src={ingredient.img}
+        alt={ingredient.name}
+      />
+      <div className={`${styles.ingrDescr} text`}>
+        <p className={styles.ingrName}>{ingredient.name}</p>
+        <p>{ingredient.measure}</p>
+      </div>
+    </li>
+  );
 };
 
 export default RecipeIngredients;
