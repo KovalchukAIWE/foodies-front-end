7396b22310d7ac8a243d62e05adc0ef1f8469fa7
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2024-06-15 16:30:22 +0300

    update RecipePagination

--- src/components/RecipePagination/RecipePagination.jsx
+++ src/components/RecipePagination/RecipePagination.jsx
@@ -1,12 +1,41 @@
+import { useEffect } from "react";
+import { useWindowScroll, useMediaQuery } from "@mantine/hooks";
 import styles from "./RecipePagination.module.css";
 
-const RecipePagination = () => {
+const RecipePagination = ({ onChangePage, currentPage, totalPage }) => {
+  const maxPageButtons = 3;
+  let startPage = Math.max(currentPage - 1, 1);
+  let endPage = Math.min(startPage + maxPageButtons - 1, totalPage);
+
+  const [, scrollTo] = useWindowScroll();
+  const tablet = useMediaQuery("(min-width: 768px)");
+  useEffect(() => {
+    scrollTo({ y: tablet ? 980 : 840 });
+  }, [currentPage]);
+
+  if (endPage - startPage < maxPageButtons - 1) {
+    startPage = Math.max(endPage - maxPageButtons + 1, 1);
+  }
+
+  const pageNumbers = Array.from(
+    { length: endPage - startPage + 1 },
+    (_, index) => startPage + index
+  );
+
   return (
     <>
       <nav className={styles.recipePagination}>
-        <button className={styles.recipePaginationButton}>1</button>
-        <button className={styles.recipePaginationButton}>2</button>
-        <button className={styles.recipePaginationButton}>3</button>
+        {pageNumbers.map((page) => (
+          <button
+            key={page}
+            className={`${styles.recipePaginationButton} ${
+              currentPage === page ? styles.active : ""
+            }`}
+            onClick={() => onChangePage(page)}
+          >
+            {page}
+          </button>
+        ))}
       </nav>
     </>
   );
