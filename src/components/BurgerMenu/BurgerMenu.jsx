430cbcb30402971489615b25347b91b9007d10ce
Andrii Kovalchuk <a.kovalchuk@gmarkg.com>
2024-06-15 21:00:53 +0300

    added burger-menu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import sprite from "../../assets/img/icons-sprite.svg";
 import styles from "./BurgerMenu.module.css";
+import { NavLink } from "react-router-dom";
 
 const BurgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,56 +10,94 @@ const BurgerMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "";
-    }
-  }, [isOpen]);
-
   return (
-    <>
-      <svg
-        className={styles.burgerMenuIcon}
-        width={28}
-        height={28}
-        onClick={toggleMenu}
-      >
-        <use href={`${sprite}#burger-menu`}></use>
-      </svg>
-      <div
-        className={`${styles.overlay} ${isOpen ? styles.open : ""}`}
-        onClick={toggleMenu}
-      ></div>
-      <div className={`${styles.burgerMenu} ${isOpen ? styles.open : ""}`}>
-        <nav>
-          <ul>
-            <li>
-              <a href="#home" onClick={toggleMenu}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#about" onClick={toggleMenu}>
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#services" onClick={toggleMenu}>
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#contact" onClick={toggleMenu}>
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
+    <div className={styles.burgerMenu}>
+      <button className={styles.burgerButton} onClick={toggleMenu}>
+        <svg
+          className={styles.burgerMenuIcon}
+          width={28}
+          height={28}
+          onClick={toggleMenu}
+        >
+          <use href={`${sprite}#burger-menu`}></use>
+        </svg>
+      </button>
+      {isOpen && (
+        <div className={`${styles.menu} ${isOpen ? styles.open : ''}`}>
+        <button className={styles.closeButton} onClick={toggleMenu}>
+          &times;
+        </button>
+        <NavLink to="/" className={styles.burgerMenuNavLink}>
+            Home
+          </NavLink>
+          <NavLink to="/" className={styles.burgerMenuNavLink}>
+            Add recipe
+          </NavLink>
       </div>
-    </>
+      )}
+    </div>
   );
 };
 
 export default BurgerMenu;
+
+// const BurgerMenu = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   useEffect(() => {
+//     if (isOpen) {
+//       document.body.style.overflow = "hidden";
+//     } else {
+//       document.body.style.overflow = "";
+//     }
+//   }, [isOpen]);
+
+//   return (
+//     <>
+//       <svg
+//         className={styles.burgerMenuIcon}
+//         width={28}
+//         height={28}
+//         onClick={toggleMenu}
+//       >
+//         <use href={`${sprite}#burger-menu`}></use>
+//       </svg>
+//       <div
+//         className={`${styles.overlay} ${isOpen ? styles.open : ""}`}
+//         onClick={toggleMenu}
+//       ></div>
+//       <div className={`${styles.burgerMenu} ${isOpen ? styles.open : ""}`}>
+//         <nav>
+//           <ul>
+//             <li>
+//               <a href="#home" onClick={toggleMenu}>
+//                 Home
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#about" onClick={toggleMenu}>
+//                 About
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#services" onClick={toggleMenu}>
+//                 Services
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#contact" onClick={toggleMenu}>
+//                 Contact
+//               </a>
+//             </li>
+//           </ul>
+//         </nav>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default BurgerMenu;
