b5476d778068e6eeb960c4288f73d2bb6f7cefa6
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2024-06-17 00:47:15 +0300

    fix AddToFavorites

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,21 +1,57 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/user/selectors";
 import { setModalSignInStatus } from "../../redux/user/slice";
 import {
+  getDataRecipeById,
   addRecipeToFavorite,
   deleteRecipeFromFavorite,
+  getUsersFavoriteRecipes,
 } from "../../services/recipes";
+import { selectFavoriteRecipes } from "../../redux/recipes/selectors.js";
 import sprite from "../../assets/img/icons-sprite.svg";
 import styles from "./RecipeCard.module.css";
 
 const RecipeCard = ({ recipe }) => {
+  const { id } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const favoriteRecipes = useSelector(selectFavoriteRecipes);
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    const checkIfFavorite = async (recipeId) => {
+      try {
+        const favorites = await getUsersFavoriteRecipes();
+        if (favorites && Array.isArray(favorites.result)) {
+          const favoriteRecipeIds = favorites.result.map(
+            (recipe) => recipe._id
+          );
+          setIsFavorite(favoriteRecipeIds.includes(recipeId));
+        } else {
+          console.error(
+            "Favorites response does not contain a valid result array:",
+            favorites
+          );
+        }
+      } catch (error) {
+        console.error("Failed to check if recipe is favorite", error);
+      }
+    };
+
+    if (isLoggedIn) {
+      checkIfFavorite(recipe._id);
+    }
+  }, [isLoggedIn, recipe._id]);
+
+  useEffect(() => {
+    setIsFavorite(
+      favoriteRecipes.some((favRecipe) => favRecipe._id === recipe._id)
+    );
+  }, [favoriteRecipes, recipe._id]);
 
   const defaultAvatar = "/src/assets/img/noUserPhoto.webp";
   const handleAuthorClick = () => {
@@ -27,6 +63,11 @@ const RecipeCard = ({ recipe }) => {
   };
 
   const handleAddToFavorites = async () => {
+    if (!isLoggedIn) {
+      dispatch(setModalSignInStatus(true));
+      return;
+    }
+
     try {
       await addRecipeToFavorite(recipe._id);
       setIsFavorite(true);
@@ -36,6 +77,11 @@ const RecipeCard = ({ recipe }) => {
   };
 
   const handleRemoveFromFavorites = async () => {
+    if (!isLoggedIn) {
+      dispatch(setModalSignInStatus(true));
+      return;
+    }
+
     try {
       await deleteRecipeFromFavorite(recipe._id);
       setIsFavorite(false);
