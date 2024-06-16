b4cfb33883062d03cd99213ca4714e09783b567c
Oleksandr <workbov@gmail.com>
2024-06-16 09:23:03 +0200

    form components

--- src/components/RecipeIngredients/RecipeIngredients.jsx
+++ src/components/RecipeIngredients/RecipeIngredients.jsx
@@ -1,20 +1,29 @@
-import styles from './RecipeIngredients.module.css';
+import css from './RecipeIngredients.module.css';
 
-const RecipeIngredients = ({ ingredient }) => {
+const RecipeIngredients = ({ ingredients }) => {
   return (
-    <li className={styles.ingrItem}>
-      <div className={styles.ingrImgBox}>
-        <img
-          src={ingredient.img}
-          alt={ingredient.name}
-          className={styles.ingrImg}
-        />
-      </div>
-      <div className={`${styles.ingrDescr} text`}>
-        <p className={styles.ingrName}>{ingredient.name}</p>
-        <p className={styles.ingrMeasure}>{ingredient.measure}</p>
-      </div>
-    </li>
+    <div className={css.recipeDetailsIngredients}>
+      <h3 className={css.recipeTitleIngredients}>Ingredients</h3>
+      <ul className={css.recipeIngredientsList}>
+        {ingredients.map((ingredient) => (
+          <li
+            key={ingredient._id}
+            className={css.ingrItem}>
+            <div className={css.ingrImgBox}>
+              <img
+                src={ingredient.img}
+                alt={ingredient.name}
+                className={css.ingrImg}
+              />
+            </div>
+            <div className={`${css.ingrDescr} text`}>
+              <p className={css.ingrName}>{ingredient.name}</p>
+              <p className={css.ingrMeasure}>{ingredient.measure}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
--- src/components/RecipeMainInfo/RecipeMainInfo.jsx
+++ src/components/RecipeMainInfo/RecipeMainInfo.jsx
@@ -1,5 +1,26 @@
-const RecipeMainInfo = () => {
-  return <div>RecipeMainInfo</div>;
+import css from './RecipeMainInfo.module.css';
+const RecipeMainInfo = ({ recipe, handleAuthorClick }) => {
+  return (
+    <div className={css.recipeTitleAndAuthor}>
+      <h2 className={css.recipeTitle}>{recipe.title}</h2>
+      <div className={css.categoryAndTime}>
+        <p className={css.recipeCategory}>{recipe.category}</p>
+        <p className={css.recipeTime}>{recipe.time} min</p>
+      </div>
+      <p className={css.recipeDescription}>{recipe.description}</p>
+      <button
+        onClick={handleAuthorClick}
+        className={css.recipeAuthor}>
+        <img
+          src={recipe.owner.avatar || 'https://via.placeholder.com/50x50'}
+          alt={recipe.owner.name}></img>
+        <div>
+          <p className={css.btnAuthorText}>Created by:</p>
+          <p className={css.recipeAuthorName}>{recipe.owner.name}</p>
+        </div>
+      </button>
+    </div>
+  );
 };
 
 export default RecipeMainInfo;
